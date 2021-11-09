8e419684502fe82d4f41c4a047e929f14db072e3
Nick Zabkar <nzabkar51@gmail.com>
2021-11-09 13:02:21 -0500

    App connects to localhost:3001

--- server.js
+++ server.js
@@ -4,4 +4,12 @@ const morgan = require("morgan");
 
 const app = express();
 
+app.use(express.static("public"));
+
 const PORT = process.env.PORT || 3001;
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/fit-track-database"
+
+app.listen(PORT, () => {
+    console.log(`Now listening on port ${PORT}`);
+});
