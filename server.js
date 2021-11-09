88abb2534c8e121f22e0427cce13c19600c59152
Nick Zabkar <nzabkar51@gmail.com>
2021-11-09 16:16:20 -0500

    Added correct database to MONGODB_URI

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    process.env.MONGODB_URI || 'mongodb://localhost/fit-track-database',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
