753ff7591856eef3c51d38c611dc1e508dcac9bd
Nick Zabkar <nzabkar51@gmail.com>
2021-11-18 00:03:20 -0500

    Changed server connection to async funcyion

--- server.js
+++ server.js
@@ -2,23 +2,28 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
-app.use(express.static("public"));
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 
-const PORT = process.env.PORT || 3001;
+app.use(express.static("public"));
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/fit-track-database',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
+connectToDb().catch(err => console.log(err));
+
+async function connectToDb() {
+  await mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+      }
+    );
+}
 
 app.listen(PORT, () => {
     console.log(`Now listening on port ${PORT}`);
