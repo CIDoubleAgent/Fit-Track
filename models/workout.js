069895440eaddafeaed25b09dab34bd602111107
Nick Zabkar <nzabkar51@gmail.com>
2021-11-18 00:04:23 -0500

    Added Workout model

---
+++ models/workout.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    excercises: [
+        {
+            type: {
+                type: String
+            },
+            name: {
+                type: String
+            },
+            weight: {
+                type: Number,
+                default: 0
+            },
+            reps: {
+                type: Number,
+                default: 0
+            },
+            sets: {
+                type: Number,
+                default: 0
+            },
+            distance: {
+                type: Number,
+                default: 0
+            },
+            duration: {
+                type: Number
+            }
+        }
+    ], 
+});
+
+const Workout = mongoose.model("workout", WorkoutSchema);
+
+module.exports = Workout;
