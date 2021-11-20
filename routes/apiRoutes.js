03b8951dbf803f46ef8a506e5a88a7767edc2099
Nick Zabkar <nzabkar51@gmail.com>
2021-11-19 19:30:39 -0500

    deploying

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,11 @@ const { Workout } = require("../models");
 const path = require("path");
 
 router.get("/stats", (req, res) => {
-    
+    res.sendFile(path.resolve() + path.join("/../public/stats.html"))
+})
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.resolve() + path.join("/../public/exercise.html"))
 })
 
 router.post("/api/workouts", (req, res) => {
@@ -12,10 +16,6 @@ router.post("/api/workouts", (req, res) => {
     workout.save(function (err) {
         res.json( workout )
     })
-})
-
-router.get("/exercise", (req, res) => {
-    res.sendFile(path.resolve() + path.join("/../public/exercise.html"))
 })
 
 router.post("/exercise", (req, res) => {
