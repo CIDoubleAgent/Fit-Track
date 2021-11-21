03a2eaab02324795436815b411a5d3cf763c8fda
Nick Zabkar <nzabkar51@gmail.com>
2021-11-20 21:54:52 -0500

    Removed "duration" to add later w/ aggregate func

--- models/workout.js
+++ models/workout.js
@@ -25,9 +25,6 @@ const workoutSchema = new Schema({
             },
             distance: {
                 type: Number,
-            },
-            duration: {
-                type: Number
             }
         }
     ], 
