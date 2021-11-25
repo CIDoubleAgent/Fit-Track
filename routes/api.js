ed22b322962add87e723cdcdd92dfc1e49948e82
Nick Zabkar <nzabkar51@gmail.com>
2021-11-25 00:33:59 -0500

    Added get route for workout data in api.js

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,31 @@
 const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 
+router.get("/workouts", async (req, res) => {
+    try {
+        const dbWorkout = await Workout.find();
+        if (dbWorkout) {
+            Workout.aggregate( [
+                    {
+                        $addFields: {
+                            totalDuration: { $sum: "$exercises.duration" }
+                        }
+                    }
+                ], (err, data) => {
+                    if (err) {
+                    res.json(err);
+                    } else {
+                        res.json(data);
+                    }
+                }
+            )
+        }
+        } catch (err) {
+            res.json(err);
+
+        }
+});
+
 
 
 module.exports = router;
