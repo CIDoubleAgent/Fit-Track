6fc62272cde8591c1469e306e631274309c2989c
Nick Zabkar <nzabkar51@gmail.com>
2021-11-25 00:42:47 -0500

    Added get route for workouts/range in api routes

--- routes/api.js
+++ routes/api.js
@@ -22,10 +22,31 @@ router.get("/workouts", async (req, res) => {
         }
         } catch (err) {
             res.json(err);
-
         }
 });
 
-
+router.get("/workouts/range", async (req, res) => {
+    try {
+        const dbWorkout = await Workout.find();
+        if (dbWorkout) {
+            Workout.aggregate( [
+                    {
+                        $addFields: {
+                            totalDuration: { $sum: "$exercises.duration" }
+                        }
+                    }
+                ], (err, data) => {
+                    if (err) {
+                    res.json(err);
+                    } else {
+                        res.json(data);
+                    }
+                }
+            )
+        }
+        } catch (err) {
+            res.json(err);
+        }
+});
 
 module.exports = router;
