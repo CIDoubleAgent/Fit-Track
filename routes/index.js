c59a1f600d1cbada50de6547fef545aa2425cb65
Nick Zabkar <nzabkar51@gmail.com>
2021-11-25 00:32:42 -0500

    Added "/" route to home routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const homeRoutes = require("./home");
 const apiRoutes = require("./api");
 
-router.use("/home", homeRoutes);
+router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
